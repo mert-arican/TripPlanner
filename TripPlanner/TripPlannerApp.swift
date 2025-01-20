ee4948bf4cc62a565e19bd09189091a5f5f6ba3d
Mert Arıcan <mert-arican@outlook.com>
2025-01-20 09:47:01 +0300

    Initial Commit

---
+++ TripPlanner/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by Mert Arıcan on 20.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
